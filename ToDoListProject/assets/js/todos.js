b36658deb20c2d3b4348783979f36f434671749e
Charles D. Redmond <charlesdredmond@gmail.com>
2017-07-03 18:52:02 -0400

    Update ToDoList project

--- ToDoListProject/assets/js/todos.js
+++ ToDoListProject/assets/js/todos.js
@@ -13,7 +13,10 @@ $("input:text").keypress(function(event){
 	if (event.which === 13){
 		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span> X </span>" + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
 	};
 });
 
+$(".fa-plus").click(function(){
+	$("input:text").fadeToggle();
+})
